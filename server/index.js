0b04d186d0d3a02288ad72617cefd50b1ac21d21
Jayasimman10 <91558509+Jayasimman10@users.noreply.github.com>
2023-12-13 00:44:55 +0530

    database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => console.log("Database connection succeeded"))
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("Server is running on the port 3000");
