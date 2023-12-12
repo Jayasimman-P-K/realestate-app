f118f27beb24f60ffb93b4cda40eb7ed3b87f7c7
Jayasimman10 <91558509+Jayasimman10@users.noreply.github.com>
2023-12-13 00:10:54 +0530

    header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Header from "./components/Header";
+
 function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div className="">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-out" element={<SignOut />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
