070ef7060fa1af1d3716aff49b7d48f75a4aa677
Jayasimman10 <91558509+Jayasimman10@users.noreply.github.com>
2023-12-13 22:46:40 +0530

    middleware

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,7 +1,7 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
 
-module.exports.signUp = async (req, res) => {
+module.exports.signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -9,6 +9,6 @@ module.exports.signUp = async (req, res) => {
     await newUser.save();
     res.status(200).json({ message: "Data succesfully stored" });
   } catch (err) {
-    res.status(500).json(err.message);
+    next(err);
   }
 };
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const authRouter = require("./routes/auth.route.js");
 const app = express();
 dotenv.config();
 
+// database connection
 mongoose
   .connect(process.env.MONGO_CONNECTION)
   .then(() => console.log("Database connection succeeded"))
@@ -16,6 +17,18 @@ app.listen(3000, () => {
   console.log("Server is running on the port 3000");
 });
 
+// routes
 app.use(express.json());
 app.use("/server/user", userRouter);
 app.use("/server/auth", authRouter);
+
+// middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
