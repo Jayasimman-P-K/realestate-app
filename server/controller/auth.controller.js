f09022d9405364ab1893e1162303d68a6e4c4b38
Jayasimman10 <91558509+Jayasimman10@users.noreply.github.com>
2023-12-14 23:51:26 +0530

    signin api route

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,5 +1,10 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
+const { errorHandler } = require("../utils/error");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
+dotenv.config();
 
 module.exports.signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,5 +15,23 @@ module.exports.signUp = async (req, res, next) => {
     res.status(200).json({ message: "Data succesfully stored" });
   } catch (err) {
     next(err);
+  }
+};
+
+module.exports.signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signUp } = require("../controller/auth.controller");
+const { signUp, signIn } = require("../controller/auth.controller");
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/signin", signIn);
 
 module.exports = router;
