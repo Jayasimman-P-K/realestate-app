35c40c8b07ebf80ac44c43d4677eec357bd689cb
Jayasimman10 <91558509+Jayasimman10@users.noreply.github.com>
2023-12-21 19:07:49 +0530

    show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -25,6 +25,9 @@ const Profile = () => {
   const [isFileUploadErr, setIsFileUploadErr] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
+
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
@@ -116,6 +119,21 @@ const Profile = () => {
     }
   };
 
+  const handleShowlisting = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/server/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className=" max-w-lg mx-auto p-3">
       <h1 className="text-center text-3xl my-7 font-semibold">Profile</h1>
@@ -203,6 +221,45 @@ const Profile = () => {
           Sign Out
         </button>
       </div>
+      <button
+        onClick={handleShowlisting}
+        className="text-green-700 w-full font-semibold"
+      >
+        Show Listing
+      </button>
+      {showListingsError && (
+        <p>{showListingsError ? "Error showing listings" : ""}</p>
+      )}
+      {userListings.length > 0 && (
+        <div>
+          <h1>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col item-center">
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
