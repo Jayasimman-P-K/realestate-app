4152f48626d46e38ac84d264bc1bf269d898e793
Jayasimman10 <91558509+Jayasimman10@users.noreply.github.com>
2023-12-22 04:19:48 +0530

    image slider in listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -155,7 +155,7 @@ const UpdateListing = () => {
       if (data.success === false) {
         setError(data.message);
       }
-      navigate(`/listings/${data._id}`);
+      navigate(`/listing/${data._id}`);
     } catch (error) {
       setError(error.message);
       setLoading(false);
