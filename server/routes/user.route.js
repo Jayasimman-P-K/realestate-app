f410ce1949ac481558f39040ed4a46ae645d54d7
Jayasimman10 <91558509+Jayasimman10@users.noreply.github.com>
2023-12-22 17:34:38 +0530

    contact landlord functionality

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,14 +4,15 @@ const {
   updateUser,
   deleteUser,
   getUserListings,
+  getUser,
 } = require("../controller/user.controller");
 const { verifyToken } = require("../utils/verifyUser");
 
 const router = express.Router();
 
-router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 
 module.exports = router;
